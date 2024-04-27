c54e4f685857b754a806a6ded773c04af36921e0
Thejellyfish1024 <alamin102410@gmail.com>
2024-04-27 16:36:47 +0600

    first commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,24 @@
-const Layout = () => {
-  return <div>Layout</div>;
+// fonts
+import { Sora } from "@next/font/google"
+import TopLeftImg from "./TopLeftImg";
+import Nav from "./Nav";
+import Header from "./Header";
+
+// font settings
+const sora = Sora({
+  subsets: ["latin"],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800']
+})
+
+const Layout = ({ children }) => {
+  return <div
+   className={`page bg-site text-white bg-cover bg-no-repeat ${sora?.variable} font-sora relative`}>
+    <TopLeftImg></TopLeftImg>
+    <Nav></Nav>
+    <Header></Header>
+    {children}
+  </div>;
 };
 
 export default Layout;
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,31 @@
+import Link from "next/link";
+import { RiBehanceLine, RiDribbbleLine, RiFacebookLine, RiInstagramLine, RiPinterestLine, RiYoutubeLine } from "react-icons/ri"
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiYoutubeLine></RiYoutubeLine>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine></RiInstagramLine>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine></RiFacebookLine>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiYoutubeLine></RiYoutubeLine>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiDribbbleLine></RiDribbbleLine>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiBehanceLine></RiBehanceLine>
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiPinterestLine></RiPinterestLine>
+      </Link>
+    </div> 
+  );
 };
 
 export default Socials;
